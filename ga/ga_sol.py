2ede47b5748aae8afbf47a83df17dccd3a282d96
jounghs3933@gmail.com <jounghs3933@gmail.com>
2023-04-09 02:15:52 +0900

    order 중복 버그 수정, 주석 추가

--- ga/ga_sol.py
+++ ga/ga_sol.py
@@ -1,13 +1,12 @@
-from ga_list import make_adj_list
 import random as rd
-# GA Algorithm Solution
+
 sol = [0 for i in range(1000)]
 
-
+# GA Algorithm Solution
 def ga_sol(adj):
+    visited_cities = set()      # the set of visited cities
     order = 0                   # the order of each city
     visit = 0                   # current city (start at 0)
-    visited_cities = set()      # the set of visited cities
 
     visited_cities.add(visit)
     # visit all citeis
@@ -16,10 +15,10 @@ def ga_sol(adj):
         for i in range(len(adj[visit])):
             if adj[visit][i] not in visited_cities:
                 visit = adj[visit][i]               # visit next city
-                sol[visit] = order                  # record the order of city
                 order += 1
+                sol[visit] = order                  # record the order of city
                 visited_cities.add(visit)
                 break
-        if i == len(adj[visit]) - 1:
-            visit = adj[visit][i]
+        if i == len(adj[visit]) - 1:                # if there is no new city to go,
+            visit = adj[visit][i]                   #  go old city and find new city
     return sol
