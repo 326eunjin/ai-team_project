642187b2b6f1212768149a424b788ba06f9b6d6a
dt313 <dt313.dev21@gmail.com>
2023-04-08 12:31:08 +0900

    add random_sol function

--- random/random_sol.py
+++ random/random_sol.py
@@ -1,7 +1,15 @@
 import random as rd
 
-def random_sol():
-    sol = [0] + rd.sample((range(1,1000)), 999) # random solution from 0 -> 999 , first citi is 0
-    return sol 
 
-print(random_sol())
+def random_sol():
+    # random solution from 0 -> 999 , first citi is 0
+    sol = [0] + rd.sample((range(1, 1000)), 999)
+    f = open('random_solution.csv', 'w')
+    # write each element of sol to the csv file
+    for i in sol:
+        f.write(str(i) + '\n')
+
+    return sol
+
+
+random_sol()
