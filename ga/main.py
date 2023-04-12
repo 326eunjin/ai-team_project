28497f92fbec9ccdd4ef39b6122b588556052fb2
326eunjin <326eunjin@naver.com>
2023-04-12 17:09:45 +0900

    우수한 자식이 있을 경우에만 선택

--- ga/main.py
+++ ga/main.py
@@ -19,7 +19,7 @@ with open('./../example_solution.csv', mode='r', newline='') as solution:
         order += 1
 
 # need to change this file into new csv sol2
-with open('./../greedy_solution.csv', mode='r', newline='') as solution:
+with open('./../random/random_solution.csv', mode='r', newline='') as solution:
 
     order = 0
     # read sol1ution sequence
@@ -68,13 +68,17 @@ def cal_total_cost(sol):
 
 # main function
 if __name__ == '__main__':
+    print(cal_total_cost(sol1))
+    print(cal_total_cost(sol2))
     sol = []
     ga = gl.Ga_sol(sol1, sol2)
-    for _ in range(100):        # GA algorithm
-        child1_tc = float("inf")
-        child2_tc = float("inf")
+    super_child1 = sol1
+    super_child2 = sol2
+    for _ in range(2):        # GA algorithm
+        child1_tc = cal_total_cost(sol1)
+        child2_tc = cal_total_cost(sol2)
         # ga_sol 여러번 돌리기
-        for _ in range(10):     # 자식 pool 만들기 (range >= 2)
+        for _ in range(1000):     # 자식 pool 만들기 (range >= 2)
             tmp_sol = ga.ga_sol()
             tmp_tc = cal_total_cost(tmp_sol)
             if (child1_tc > tmp_tc) | (child2_tc > tmp_tc):          # 가장 우수한 자식쌍 유지
@@ -85,6 +89,6 @@ if __name__ == '__main__':
                     super_child2 = tmp_sol
                     child2_tc = tmp_tc
         ga.generation_change(super_child1, super_child2)
-    sol = (super_child1 if child1_tc > child2_tc else super_child2)
+    sol = (super_child1 if child1_tc < child2_tc else super_child2)
     make_csv("ga_solution.csv", sol)
     print(cal_total_cost(sol))
