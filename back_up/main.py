f021a2240045aea2f428a362246e4f9a77bfde3b
326eunjin <326eunjin@naver.com>
2023-04-13 17:04:45 +0900

    main class화

--- back_up/main.py
+++ back_up/main.py
@@ -1,40 +1,6 @@
 import random as rd
 import csv
 import numpy as np
-# import ga_sol as gl
-
-# given cities
-cities = []
-# sol1ution
-sol1 = [0 for _ in range(1001)]
-sol2 = [0 for _ in range(1001)]
-
-# need to change this file into new csv sol1
-with open('../example_solution.csv', mode='r', newline='') as solution:
-
-    order = 0
-    # read sol1ution sequence
-    reader = csv.reader(solution)
-    for row in reader:
-        sol1[order] = int(row[0])
-        order += 1
-
-# need to change this file into new csv sol2
-with open('../random/random_solution.csv', mode='r', newline='') as solution:
-
-    order = 0
-    # read sol1ution sequence
-    reader = csv.reader(solution)
-    for row in reader:
-        sol2[order] = int(row[0])
-        order += 1
-
-# get TSP city map
-with open('../2023_AI_TSP.csv', mode='r', newline='', encoding='utf-8-sig') as tsp:
-    # read TSP city map
-    reader = csv.reader(tsp)
-    for row in reader:
-        cities.append(row)
 
 # Euclidean distance measuring function
 
@@ -44,29 +10,65 @@ def distance(x, y):
     return dist
 
 
-def make_csv(path, sol):
-    sol = sol[:1000]
-    f = open(path, 'w')
-    # write each element of sol to the csv file
-    for i in sol:
-        f.write(str(i) + '\n')
+class Main:
+    # given cities
+    def __init__(self):
+        self.cities = []
+# sol1ution
+        self.sol1 = [0 for _ in range(1001)]
+        self.sol2 = [0 for _ in range(1001)]
+# with open('./../example_solution.csv', mode='r', newline='') as solution:
+        with open('./../example_solution.csv', mode='r', newline='') as solution:
 
+            order = 0
+        # read sol1ution sequence
+            reader = csv.reader(solution)
+            for row in reader:
+                self.sol1[order] = int(row[0])
+                order += 1
 
-def cal_total_cost(sol):
-    # evaluate solution cost
-    total_cost = 0
-    for idx in range(len(sol)-1):
-        # get city positions
-        pos_city_1 = [float(cities[sol[idx]][0]), float(cities[sol[idx]][1])]
-        pos_city_2 = [float(cities[sol[idx+1]][0]),
-                      float(cities[sol[idx+1]][1])]
+    # need to change this file into new csv sol2
+        with open('./../random/random_solution.csv', mode='r', newline='') as solution:
 
-    # distance calculation
-        dist = distance(pos_city_1, pos_city_2)
+            order = 0
+            # read sol1ution sequence
+            reader = csv.reader(solution)
+            for row in reader:
+                self.sol2[order] = int(row[0])
+                order += 1
 
-    # accumulation
-        total_cost += dist
-    return total_cost
+        # get TSP city map
+        with open('./../2023_AI_TSP.csv', mode='r', newline='', encoding='utf-8-sig') as tsp:
+            # read TSP city map
+            reader = csv.reader(tsp)
+            for row in reader:
+                self.cities.append(row)
+
+        # Euclidean distance measuring function
+
+    def make_csv(self, path, sol):
+        f = open(path, 'w')
+        # write each element of sol to the csv file
+        for i in sol:
+            f.write(str(i) + '\n')
+
+    def cal_total_cost(self, sol):
+        # evaluate solution cost
+        total_cost = 0
+        for idx in range(len(sol)-1):
+            # get city positions
+            pos_city_1 = [float(self.cities[sol[idx]][0]),
+                          float(self.cities[sol[idx]][1])]
+            pos_city_2 = [float(self.cities[sol[idx+1]][0]),
+                          float(self.cities[sol[idx+1]][1])]
+
+        # distance calculation
+            dist = distance(pos_city_1, pos_city_2)
+
+        # accumulation
+            total_cost += dist
+        return total_cost
+        # print('final cost: '+str(total_cost))
 
 
 class Ga_sol:
@@ -121,22 +123,21 @@ class Ga_sol:
                     break
                 elif i == len(adj[visit]) - 1:
                     visit = adj[visit][rd.choice(range(len(adj[visit])))]
+            # 마지막 남은 도시 탐색
             if len(self.visited_cities) == 999:
-                # 마지막 남은 도시 탐색
                 visit = (set(list(range(1000))) - self.visited_cities).pop()
                 sol.append(visit)
                 self.visited_cities.add(visit)
-        sol.append(int(0))
         return sol
 
     # centroid 업데이트
     def update_centroid(self, visit):
         if len(self.visited_cities) == 999:
             return [0, 0]
-        return [((float(self.centroid[0]) * (1000 - len(self.visited_cities))) - float(cities[visit][0]))/(999 - len(self.visited_cities)),
-                ((float(self.centroid[1]) * (1000 - len(self.visited_cities))) - float(cities[visit][1]))/(999 - len(self.visited_cities))]
+        return [((float(self.centroid[0]) * (1000 - len(self.visited_cities))) - float(self.cities[visit][0]))/(999 - len(self.visited_cities)),
+                ((float(self.centroid[1]) * (1000 - len(self.visited_cities))) - float(self.cities[visit][1]))/(999 - len(self.visited_cities))]
 
-    # heuristic 정렬 기준
+    # heuristic 정렬 기준 (시작점, 무게중심 거리비)
     def sort_by(self, city1, city2):
         dist1 = distance([float(city1[0]), float(city1[1])],
                          [float(city2[0]), float(city2[0])])
@@ -147,26 +148,27 @@ class Ga_sol:
     def heuristic(self, adj, visit):
         centroid = self.update_centroid(visit)
         # 시작점과 무게중심이 같아질 경우
-        return sorted(adj[visit], key=lambda x: self.sort_by(cities[x], centroid))
+        return sorted(adj[visit], key=lambda x: self.sort_by(self.cities[x], centroid))
 
 
 # main function
 if __name__ == '__main__':
-    print(cal_total_cost(sol1))
-    print(cal_total_cost(sol2))
+    main = Main()
+    print(main.cal_total_cost(main.sol1))
+    print(main.cal_total_cost(main.sol2))
     tmp_sol = []
     sol = []
-    ga = Ga_sol(sol1, sol2, cities)
-    super_child1 = sol1
-    super_child2 = sol2
-    for _ in range(100):        # GA algorithm
+    ga = Ga_sol(main.sol1, main.sol2, main.cities)
+    super_child1 = main.sol1
+    super_child2 = main.sol2
+    for _ in range(10):        # GA algorithm
         # 세대교체
-        child1_tc = cal_total_cost(sol1)
-        child2_tc = cal_total_cost(sol2)
+        child1_tc = main.cal_total_cost(main.sol1)
+        child2_tc = main.cal_total_cost(main.sol2)
         # ga_sol 여러번 돌리기
         for _ in range(10):     # 자식 pool 만들기 (range >= 2)
             tmp_sol = ga.ga_sol()
-            tmp_tc = cal_total_cost(tmp_sol)
+            tmp_tc = main.cal_total_cost(tmp_sol)
             if (child1_tc > tmp_tc) | (child2_tc > tmp_tc):          # 가장 우수한 자식쌍 유지
                 if child1_tc > child2_tc:
                     super_child1 = tmp_sol
@@ -176,5 +178,5 @@ if __name__ == '__main__':
                     child2_tc = tmp_tc
         ga.generation_change(super_child1, super_child2)
         sol = (super_child1 if child1_tc < child2_tc else super_child2)
-    make_csv("ga_solution.csv", sol)
-    print(cal_total_cost(sol))
+    main.make_csv("ga_solution.csv", sol)
+    print(main.cal_total_cost(sol))
