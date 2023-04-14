b112215d3d9a3f1ca16a95d3bf857f55159da145
326eunjin <326eunjin@naver.com>
2023-04-14 17:40:57 +0900

    루프 사이클

--- tree/main.py
+++ tree/main.py
@@ -18,8 +18,10 @@ class Ga_sol:
         self.adj = self.make_adj_list()
         self.dist_adj = self.make_dist_adj(dist_bound)
         self.visited_cities = set()
+        self.unvisited_cities = set(list(range(1000)))
+    # 세대 교체 메소드
 
-    def generation_change(self, super_child1, super_child2):   # 세대 교체 메소드
+    def generation_change(self, super_child1, super_child2):
         self.sol1 = super_child1
         self.sol2 = super_child2
         self.adj = self.make_adj_list()
@@ -34,7 +36,6 @@ class Ga_sol:
             tmp_set.add(self.sol2[(self.sol2.index(i)+1) % 1000])
             adjacency_list[i] = list(tmp_set)
         return adjacency_list
-        # heuristic 정렬 기준 (다음 점까지의 거리)
 
     def cal_total_cost(self, sol):
         # evaluate solution cost
@@ -46,33 +47,34 @@ class Ga_sol:
             pos_city_2 = [float(self.cities[sol[idx+1]][0]),
                           float(self.cities[sol[idx+1]][1])]
 
-        # distance calculation
+            # distance calculation
             dist = distance(pos_city_1, pos_city_2)
 
-        # accumulation
+            # accumulation
             total_cost += dist
         return total_cost
-        # print('final cost: '+str(total_cost))
 
     def cal_four_points(self, x, y):
-
+        unvisited_cities = set(list(range(1000))) - self.visited_cities
         q_1 = []
         q_2 = []
         q_3 = []
         q_4 = []
         tmp = 0
         ret = []
-        # ret은 2차원리스트인데 가장 작은 점을 포함
-        # # 여기서부터 이제 사분면을 기준으로 가장 먼점을 계산하는 함수
-        for i, j in self.cities:
-            if (float(i) > x and float(j) > y):
-                q_1.append([float(i), float(j)])
-            elif (float(i) < x and float(j) > y):
-                q_2.append([float(i), float(j)])
-            elif (float(i) < x and float(j) < y):
-                q_3.append([float(i), float(j)])
-            elif (float(i) > x and float(j) < y):
-                q_4.append([float(i), float(j)])
+        # ret은 가장 작은 점을 포함하는 2차원리스트
+        # 사분면을 기준으로 가장 먼점을 계산
+        for n in unvisited_cities:
+            if n not in self.visited_cities:
+                i, j = self.cities[n][0], self.cities[n][1]
+                if (float(i) > x and float(j) > y):
+                    q_1.append([float(i), float(j)])
+                elif (float(i) < x and float(j) > y):
+                    q_2.append([float(i), float(j)])
+                elif (float(i) < x and float(j) < y):
+                    q_3.append([float(i), float(j)])
+                elif (float(i) > x and float(j) < y):
+                    q_4.append([float(i), float(j)])
 
         max_1 = 0
         for i in q_1:
@@ -124,29 +126,27 @@ class Ga_sol:
             ret = distance(tmp[0], tmp[1]) \
                 + distance(tmp[1], tmp[2]) + distance(tmp[0], tmp[2])
         # 3개일때
-        elif (len == 3):
-            # if (point_list[0]==0):
-            #     ret = distance(point_list[1],[x,y])+distance(point_list[1],point_list[2])+distance(point_list[2],point_list[3])+distance([x,y],point_list[3])
-            # elif (point_list[1]==0):
-            #     ret = distance(point_list[0],[x,y])+distance([x,y],point_list[2])+distance(point_list[2],point_list[3])+distance(point_list[3],point_list[0])
-            # elif (point_list[2]==0):
-            #     ret = distance(point_list[0],point_list[1])+distance([x,y],point_list[1])+distance(point_list[3],[x,y])+distance(point_list[3],point_list[0])
-            # elif (point_list[3]=='[]'):
-            #     ret = distance(point_list[0],[x,y])+distance(point_list[1],point_list[0])+distance(point_list[2],point_list[1])+distance(point_list[2],[x,y])
-            if 0 in point_list:
-                idx = point_list.index(0)
-                indices = [idx-1, idx+1, idx-2, idx+2]
-                ret = sum(distance(point_list[i], point_list[j]) for i, j in zip(
-                    indices, indices[1:]+indices[:1]))
-
+        elif (length == 3):
+            if (point_list[0] == 0):
+                ret = distance(point_list[1], [x, y])+distance(point_list[1], point_list[2])+distance(
+                    point_list[2], point_list[3])+distance([x, y], point_list[3])
+            elif (point_list[1] == 0):
+                ret = distance(point_list[0], [x, y])+distance([x, y], point_list[2])+distance(
+                    point_list[2], point_list[3])+distance(point_list[3], point_list[0])
+            elif (point_list[2] == 0):
+                ret = distance(point_list[0], point_list[1])+distance([x, y], point_list[1])+distance(
+                    point_list[3], [x, y])+distance(point_list[3], point_list[0])
+            elif (point_list[3] == 0):
+                ret = distance(point_list[0], [x, y])+distance(point_list[1], point_list[0])+distance(
+                    point_list[2], point_list[1])+distance(point_list[2], [x, y])
         # 4개일때
         else:
             dist = []
             tmp = []
             for i in range(4):
                 dist.append([distance([x, y], point_list[i]), i])
-                # dist에는 [거리,i]이렇게 들어감 i는 분면 근데 i=0인게 1사분면
-                # dist.sort()[0][0]#가장 짧은 거리
+            # dist에는 [거리,i]이렇게 들어감 i는 분면 근데 i=0인게 1사분면
+            # dist.sort()[0][0]#가장 짧은 거리
             sorted_dist = sorted(dist, key=lambda x: x[0], reverse=False)
             if (sorted_dist[1][1] % 2 == sorted_dist[0][1] % 2):  # 대각선인경우
                 tmp.append(sorted_dist[2])
@@ -171,8 +171,6 @@ class Ga_sol:
         return distance([float(city1[0]), float(city1[1])],
                         [float(city2[0]), float(city2[1])]) < self.dist_bound
 
-    # GA Algorithm Solution
-
     def mutation(self, visit):
         while True:
             for i in range(len(self.dist_adj[visit])):
@@ -182,32 +180,56 @@ class Ga_sol:
 
     # GA Algorithm Solution
     def ga_sol(self, mutation_prob):
-        sol = [0 for _ in range(1001)]
-        visit = 0                   # current city (start at 0)
+        sol = [0 for _ in range(1001)]  # 결과값
+        visit = 0   # current city (start at 0)
         order = 0
+        flag = True
+        self.visited_cities = set()  # 이미 간 도시인지 확인/여기선 초기화
+        self.unvisited_cities = set(list(range(1000)))
+        self.visited_cities.add(visit)  # 현재 도시 집합에 추가
+        self.unvisited_cities.remove(visit)
 
-        self.visited_cities = set()
-        self.visited_cities.add(visit)
         # visit all cities
-        while len(self.visited_cities) < 1000:
-            print(len(self.visited_cities))
-            # rd.shuffle(self.adj[visit])
-            # visit 현재 도시 x가 갈 수 있는 모든 도시
+        while len(self.visited_cities) < 1000:  # 종료조건 모든 점 순회
+            print(len(self.visited_cities), visit)
+            # visit 현재 도시
+            # x가 갈 수 있는 모든 도시
             self.adj[visit] = sorted(self.adj[visit], key=lambda x:
                                      self.cal_heuristic(self.cities[x])+self.cal_total_cost(sol))
-            for i in range(len(self.adj[visit])):
-                if self.adj[visit][i] not in self.visited_cities:
-                    if rd.random() > mutation_prob or self.isin_bound(self.cities[visit], self.cities[self.adj[visit][i]]):
-                        visit = self.adj[visit][i]
-                    else:
-                        visit = self.mutation(visit)
+            # g(n)+h(n) 기준 정렬
+            for i in range(len(self.adj[visit])):  # 인접도시 돌면서
+                if self.adj[visit][i] not in self.visited_cities:  # 인접도시가 방문하지 않은 도시면
+                    # 인접 도시와의 거리가 dist_bound보다 작으면 mutation_prob 확률로 이동
+                    if (flag):
+                        if rd.random() > mutation_prob or self.isin_bound(self.cities[visit], self.cities[self.adj[visit][i]]):
+                            visit = self.adj[visit][i]
+                    # 아니면 돌연변이 발생
+                        else:
+                            visit = self.mutation(visit)
+                    # 순서++
                     order += 1
-                    # record the order of city
                     sol[order] = visit
+                    # 현재 도시 추가
                     self.visited_cities.add(visit)
+                    self.unvisited_cities.discard(visit)
+                    flag = True
+                    # 갔다온 도시 목록에도 추가
                     break
+                # 내가 갈 수 있는 도시가 모두 이미 방문한 도시이면
                 elif i == len(self.adj[visit]) - 1:
-                    visit = rd.choice(self.adj[visit])
+                    visit = rd.choice(list(self.unvisited_cities))
+                    flag = False
+                    # visit = self.mutation(
+                    #     rd.choice(self.dist_adj[visit]))
+                    # min = float("inf")
+                    # minarg = visit
+                    # for city in self.unvisited_cities:
+                    #     tmp = distance([float(self.cities[city][0]), float(self.cities[city][1])],
+                    #                    [float(self.cities[visit][0]), float(self.cities[visit][1])])
+                    #     if min > tmp:
+                    #         min = tmp
+                    #         minarg = city
+                    # visit = minarg
         return sol
 
 
@@ -215,20 +237,20 @@ class Main:
     # given cities
     def __init__(self):
         self.cities = []
-# sol1ution
+        # sol1ution
         self.sol1 = [0 for _ in range(1001)]
         self.sol2 = [0 for _ in range(1001)]
-# with open('./../example_solution.csv', mode='r', newline='') as solution:
+        # with open('./../example_solution.csv', mode='r', newline='') as solution:
         with open('/Users/jang-eunjin/Desktop/ai-team_project/example_solution.csv', mode='r', newline='') as solution:
 
             order = 0
-        # read sol1ution sequence
+            # read sol1ution sequence
             reader = csv.reader(solution)
             for row in reader:
                 self.sol1[order] = int(row[0])
                 order += 1
 
-    # need to change this file into new csv sol2
+        # need to change this file into new csv sol2
         with open('/Users/jang-eunjin/Desktop/ai-team_project/random/random_solution.csv', mode='r', newline='') as solution:
 
             order = 0
@@ -244,8 +266,6 @@ class Main:
             reader = csv.reader(tsp)
             for row in reader:
                 self.cities.append(row)
-
-        # Euclidean distance measuring function
 
     def make_csv(self, path, sol):
         f = open(path, 'w')
@@ -263,32 +283,33 @@ class Main:
             pos_city_2 = [float(self.cities[sol[idx+1]][0]),
                           float(self.cities[sol[idx+1]][1])]
 
-        # distance calculation
+            # distance calculation
             dist = distance(pos_city_1, pos_city_2)
 
-        # accumulation
+            # accumulation
             total_cost += dist
         return total_cost
-        # print('final cost: '+str(total_cost))
 
 
 # main function
 if __name__ == '__main__':
     sol = []
-    dist_bound = 15
-    mutation_prob = 1
+    dist_bound = 25
+    mutation_prob = 0.5
     main = Main()
     ga = Ga_sol(main.sol1, main.sol2, main.cities, dist_bound)
-    for _ in range(1):     # GA algorithm
+    # 세대 교체
+    for _ in range(5):
         super_child1 = ga.sol1
         super_child2 = ga.sol2
         child1_tc = main.cal_total_cost(super_child1)
         child2_tc = main.cal_total_cost(super_child2)
-        # ga_sol 여러번 돌리기
-        for _ in range(2):    # 자식 pool 만들기
+        # 자식 pool 만들기
+        for _ in range(10):
             tmp_sol = ga.ga_sol(mutation_prob)
             tmp_tc = main.cal_total_cost(tmp_sol)
-            if (child1_tc > tmp_tc) or (child2_tc > tmp_tc):          # 가장 우수한 자식쌍 유지
+            # 가장 우수한 자식쌍 유지
+            if (child1_tc > tmp_tc) or (child2_tc > tmp_tc):
                 if child1_tc > child2_tc:
                     super_child1 = tmp_sol
                     child1_tc = tmp_tc
@@ -298,5 +319,5 @@ if __name__ == '__main__':
         ga.generation_change(super_child1, super_child2)
     sol = (super_child1 if child1_tc < child2_tc else super_child2)
     sol = sol[:1000]
-    main.make_csv("ga_solution.csv", sol)
+    main.make_csv("tree_solution.csv", sol)
     print(main.cal_total_cost(sol))
